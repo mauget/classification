83c0a2e76bca311c3809ffe34ac0ed6419769c9d
mauget <mauget@mindspring.com>
2017-03-06 16:40:19 -0500

    Format

--- groking-dl-back-prop.py
+++ groking-dl-back-prop.py
@@ -31,9 +31,10 @@ for iteration in xrange(300):
     layer_2_error = 0
     for i in xrange(len(streetlights)):
         layer_0 = streetlights[i : i+1]
-        layer_1 = relu(np.dot(layer_0,weights_0_1))
 
+        layer_1 = relu(np.dot(layer_0,weights_0_1))
         layer_2 = np.dot(layer_1,weights_1_2)
+
         layer_2_error += np.sum((layer_2 - walk_vs_stop[i:i+1]) ** 2)
 
         layer_2_delta = (layer_2 - walk_vs_stop[i : i+1])
