d1f6add14dff9d28d7b22b0a59e8b7afa75b1d12
mauget <mauget@mindspring.com>
2017-03-06 19:38:21 -0500

    Removed one numpy.T

--- groking-dl-back-prop.py
+++ groking-dl-back-prop.py
@@ -1,17 +1,15 @@
 
 import numpy as np
-import math
 
 np.random.seed(1)
 
 def relu(x):
-    # returns x if x > 0
-    # return 0 otherwise
-    return (x > 0) * x
+    return (x > 0) * x  # returns x if x > 0
+                        # return 0 otherwise
 
 def relu2deriv(output):
-    return output>0 # returns 1 for input > 0
-                    # return 0 otherwise
+    return output > 0   # returns 1 for input > 0
+                        # return 0 otherwise
 
 streetlights = np.array(
     [[ 1, 0, 1 ],
@@ -20,11 +18,12 @@ streetlights = np.array(
      [ 1, 1, 1 ]
     ] )
 
-walk_vs_stop = np.array([[ 1, 1, 0, 0]]).T
+walk_vs_stop = np.array( [ [1], [1], [0], [0] ] )
+
 alpha = 0.5
 hidden_size = 4
 
-weights_0_1 = 2 * np.random.random((3,hidden_size)) - 1
+weights_0_1 = 2 * np.random.random((3, hidden_size)) - 1
 weights_1_2 = 2 * np.random.random((hidden_size,1)) - 1
 
 line_num = 0
@@ -37,11 +36,15 @@ for iteration in xrange(300):
         layer_1 = relu(np.dot(layer_0, weights_0_1))
         layer_2 = np.dot(layer_1, weights_1_2)
 
+        # layer_2 = relu(layer_0.dot(weights_0_1)).dot(weights_1_2)
+
+        # For logging:
         layer_2_error += np.sum((layer_2 - walk_vs_stop[i : i+1]) ** 2)
 
         layer_2_delta = (layer_2 - walk_vs_stop[i : i+1])
         layer_1_delta = layer_2_delta.dot(weights_1_2.T) * relu2deriv(layer_1)
 
+        # Seek lowest error
         weights_1_2 -= alpha * layer_1.T.dot(layer_2_delta)
         weights_0_1 -= alpha * layer_0.T.dot(layer_1_delta)
 
