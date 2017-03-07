b6cb60f877aa2e27d36dbf2ad5008aef51641639
mauget <mauget@mindspring.com>
2017-03-06 19:06:06 -0500

    Format

--- groking-dl-back-prop.py
+++ groking-dl-back-prop.py
@@ -1,11 +1,13 @@
 
 import numpy as np
+import math
 
 np.random.seed(1)
 
 def relu(x):
-    return (x > 0) * x # returns x if x > 0
-                    # return 0 otherwise
+    # returns x if x > 0
+    # return 0 otherwise
+    return (x > 0) * x
 
 def relu2deriv(output):
     return output>0 # returns 1 for input > 0
@@ -31,6 +33,10 @@ for iteration in xrange(300):
     layer_2_error = 0
     for i in xrange(len(streetlights)):
         layer_0 = streetlights[i : i+1]
+
+        # print( str(np.dot(layer_0, weights_0_1).shape ) )
+        # print( str(layer_0)  )
+
         layer_1 = relu(np.dot(layer_0, weights_0_1))
         layer_2 = np.dot(layer_1, weights_1_2)
 
@@ -42,6 +48,8 @@ for iteration in xrange(300):
         weights_1_2 -= alpha * layer_1.T.dot(layer_2_delta)
         weights_0_1 -= alpha * layer_0.T.dot(layer_1_delta)
 
+        # print("\n")
+
     if(iteration % 10 == 9):
         line_num += 10
-        print str(line_num) + ". Error:" + str(layer_2_error)
+        print str(line_num) + ". Error: " + str(layer_2_error)
