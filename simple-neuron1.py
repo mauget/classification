30ff1f13f89a746be1be6503a8f67a7e26309510
mauget <mauget@mindspring.com>
2017-03-12 08:08:03 -0400

    signed huge initial weight

--- simple-neuron1.py
+++ simple-neuron1.py
@@ -1,14 +1,16 @@
 import random
 
-# This weight is the neural value -- the "knowledge"
-# At first we're Sgt Shultz: we know nuthing!
-weight = 300 * random.random()
+# This signed weight is the neural value -- the "knowledge"
+# At first we're Sgt Shultz: we know n0-thing!
+sign = 1 if (random.random() > 0.5) else -1
+weight = 1e3 * random.random() * sign
+
 initial_weight = weight
 
 # We seek a neuron that predicts a value 0f 0.8 for an input of 0.5
 goal = 0.8
 
-# It's a demo! Most neurons take an input array instead of a scalar.
+# Demo. Most neurons take an input array instead of a scalar.
 input_value = 0.5
 
 diff_fuzz = 1e-25
@@ -21,19 +23,19 @@ def neuron(input):
 counter = 0
 circuit_breaker = int(1e5)
 
-for x in range(0, circuit_breaker):
+for x in range(circuit_breaker):
     prediction = neuron(input_value)
 
-    # Adjust weight from delta slope magnitube and sign
+    # Adjust weight using value of delta slope and sign
     delta = prediction - goal
-    weight -= delta * input_value
+    correction = delta * input_value
+    weight -= correction
 
     counter += 1
-    print(str(counter) + ". Prediction " + str(prediction) + " via weight " + str(weight) + ". Seeking goal " + str(goal))
+    print(str(counter) + ". Prediction " + str(prediction) + " using weight " + str(weight) + ". Seeking goal " + str(goal))
 
     circuit_breaker -= 1
-    error = delta ** 2
-    if error < diff_fuzz:
+    if delta ** 2 < diff_fuzz:
         break
 
 
